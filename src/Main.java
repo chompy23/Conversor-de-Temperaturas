d16129f5b02c2cbcd291445beb4db1eda666a459
chompy23 <sdefederico1@gmail.com>
2024-04-05 16:27:44 -0300

    Primer commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner celcius = new Scanner(System.in);
         System.out.println("Ingrese la temperatura en Grados Celcius");
-        double TemperaturaGradosCelcius = celcius.nextDouble();
-        double TenperaturaEnGradosFarenheit = (TemperaturaGradosCelcius * 1.8) + 32;
-        System.out.println(" La temperatura en Grados Farenheit es : " + (Math.round(TenperaturaEnGradosFarenheit * 100.0) / 100.0));
+        double temperaturaGradosCelcius = celcius.nextDouble();
+        double tenperaturaEnGradosFarenheit = (temperaturaGradosCelcius * 1.8) + 32;
+        System.out.println(" La temperatura en Grados Farenheit es : " + (Math.round(tenperaturaEnGradosFarenheit * 100.0) / 100.0) + "°F");
+        int  temperaturaFinalGradosFarenheit = (int) tenperaturaEnGradosFarenheit;
+        System.out.println(" La temperatura en Grados Farenheit es : " + temperaturaFinalGradosFarenheit + "°F");
     }
 }
